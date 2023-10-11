e87e53c8e4952bd59abf189d112d025b48daa324
Shoon <qortmd7777@gmail.com>
2023-10-11 21:32:05 +0900

    chore: Spring3 프로젝트 초기 세팅

---
+++ src/main/java/com/example/hagimabackend/HagimaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hagimabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HagimaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HagimaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hagimabackend/HagimaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hagimabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HagimaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
