4e05d3ca46fe8b045a6e52faeb4a1e1606ba01cb
Shoon <qortmd7777@gmail.com>
2023-11-15 16:10:47 +0900

    feat: Swagger Setting

---
+++ src/main/java/com/example/hagimabackend/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.example.hagimabackend.config;
+
+import io.swagger.v3.oas.models.Components;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Hagima Main Backend API").description("하지마 메인 백엔드 서버 API 문서").version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("UUID")));
+    }
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("API")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
--- src/main/java/com/example/hagimabackend/controller/HealthCheckController.java
+++ src/main/java/com/example/hagimabackend/controller/HealthCheckController.java
@@ -1,14 +1,18 @@
 package com.example.hagimabackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
+@Tag(name="Health", description = "LB Health Check API")
 public class HealthCheckController {
 
     @GetMapping("")
+    @Operation(summary = "로드밸런서 서버 상태 확인용 API")
     public String healthCheck() {
         return "Server is healthy.";
     }
