91a1f8bb4f974b973f215c51a40a2804ac1d7bab
Shoon <qortmd7777@gmail.com>
2023-11-23 02:17:30 +0900

    chore: Cors Setting Fix

--- src/main/java/com/example/hagimabackend/util/security/SecurityConfig.java
+++ src/main/java/com/example/hagimabackend/util/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
             CorsConfiguration config = new CorsConfiguration();
 
             config.setAllowCredentials(true);
-            config.setAllowedOrigins(List.of("http://localhost:3000"));
+            config.setAllowedOrigins(List.of("http://localhost:3000", "https://hagima.vercel.app"));
             config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
             config.setAllowedHeaders(List.of("*"));
             config.setExposedHeaders(List.of("*"));
