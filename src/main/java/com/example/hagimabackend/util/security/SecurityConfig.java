64a3125df95a6169e2ad152ee94e90bffca83d98
Shoon <qortmd7777@gmail.com>
2023-11-23 03:19:57 +0900

    chore: Security Logging

--- src/main/java/com/example/hagimabackend/util/security/SecurityConfig.java
+++ src/main/java/com/example/hagimabackend/util/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
             CorsConfiguration config = new CorsConfiguration();
 
             config.setAllowCredentials(true);
-            config.setAllowedOrigins(List.of("http://localhost:3000", "https://hagima.vercel.app"));
+            config.setAllowedOrigins(List.of("http://localhost:3000", "https://hagima.vercel.app", "https://hgm-main.p-e.kr"));
             config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
             config.setAllowedHeaders(List.of("*"));
             config.setExposedHeaders(List.of("*"));
