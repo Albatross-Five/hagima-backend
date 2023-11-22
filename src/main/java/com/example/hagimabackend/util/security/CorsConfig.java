5451e22cfacf98d250c7769742788a9cd3f8e667
Shoon <qortmd7777@gmail.com>
2023-11-23 01:33:01 +0900

    chore: Cors Setting Fix

---
+++ src/main/java/com/example/hagimabackend/util/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.hagimabackend.util.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 모든 오리진 허용 (보안상 주의)
+        corsConfiguration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        corsConfiguration.addAllowedHeader("*"); // 모든 헤더 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+    }
+}
